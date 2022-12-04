eb3101876e54c63a69874866e367de99650b95aa
Colin Barry <colinbarry@gmail.com>
2022-12-04 19:18:36 +0000

    feat: solve day 4

---
+++ 04/part2.cpp
@@ -0,0 +1,19 @@
+// g++ -std=c++17 part2.cpp && ./a.out < test
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+
+int main()
+{
+    std::cout << std::count_if(
+        std::istream_iterator<std::string>(std::cin),
+        std::istream_iterator<std::string>(),
+        [](std::string const& s) {
+            int lfrom, lto, rfrom, rto;
+            sscanf(s.c_str(), "%i-%i,%i-%i", &lfrom, &lto, &rfrom, &rto);
+            return !(lto < rfrom || lfrom > rto);
+        }) << "\n";
+}
