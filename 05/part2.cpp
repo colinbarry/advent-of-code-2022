ddb1faa07fdc879c6ba7dcbf634a32c52828a213
Colin Barry <colinbarry@gmail.com>
2022-12-05 21:10:18 +0000

    feat: solve day 5

---
+++ 05/part2.cpp
@@ -0,0 +1,55 @@
+// g++ -std=c++17 part2.cpp && ./a.out < test
+#include <algorithm>
+#include <cstdio>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::string s;
+    std::map<size_t, std::deque<char>> columns;
+
+    while (std::getline(std::cin, s))
+    {
+        if (std::find(s.cbegin(), s.cend(), '[') == s.cend())
+            break;
+
+        for (size_t i = 1, index = 1; i < s.size(); i += 4, ++index)
+        {
+            char const c = s[i];
+            if (::isalpha(c))
+            {
+                columns[index].push_front(c);
+            }
+        }
+    }
+
+    std::string skipline;
+    std::getline(std::cin, skipline);
+
+    while (std::getline(std::cin, s))
+    {
+        int num, from, to;
+        sscanf(s.c_str(), "move %i from %i to %i", &num, &from, &to);
+
+        auto& fromColumn = columns[from];
+        auto& toColumn = columns[to];
+
+        std::vector<char> buffer;
+        std::copy(fromColumn.cend() - num,
+                  fromColumn.cend(),
+                  std::back_inserter(buffer));
+        fromColumn.erase(fromColumn.end() - num, fromColumn.end());
+
+        toColumn.insert(toColumn.end(), buffer.cbegin(), buffer.cend());
+    }
+
+    for (auto&& each : columns)
+    {
+        std::cout << each.second.back();
+    }
+    std::cout << "\n";
+}
