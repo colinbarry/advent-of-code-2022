2af8a17db455a1f114264445a48ca486220db514
Colin Barry <colinbarry@gmail.com>
2022-12-20 20:16:10 +0000

    feat: solve day 17 part 1

---
+++ 17/part1.cpp
@@ -0,0 +1,127 @@
+// g++ -std=c++17 part2.cpp && ./a.out < test
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+using RockDef = std::vector<std::pair<int, int>>;
+
+static const std::array<RockDef, 5> rockDefs
+    = {RockDef{{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+       RockDef{{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}},
+       RockDef{{2, 0}, {2, 1}, {0, 2}, {1, 2}, {2, 2}},
+       RockDef{{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+       RockDef{{0, 0}, {0, 1}, {1, 0}, {1, 1}}};
+
+struct Rock
+{
+    RockDef blocks;
+    int height;
+};
+
+std::array<Rock, 5> rocks;
+
+int main()
+{
+    std::transform(rockDefs.cbegin(), rockDefs.cend(), rocks.begin(), [](RockDef const& def) {
+        return Rock{def,
+                    std::accumulate(def.cbegin(),
+                                    def.cend(),
+                                    0,
+                                    [](int const height, auto const& pair) {
+                                        return std::max(height, pair.second + 1);
+                                    })};
+    });
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::set<std::pair<int, int>> fixed;
+
+    auto const top = [&] {
+        return std::accumulate(fixed.cbegin(),
+                               fixed.cend(),
+                               0,
+                               [](int const height, auto const& pair) {
+                                   return std::min(height, pair.second);
+                               });
+    };
+
+    auto const testSpace = [&](int const x, int const y) {
+        return x >= 0 && x < 7 && y < 0 && fixed.count(std::make_pair(x, y)) == 0;
+    };
+
+    bool spawnNewRock = true;
+    int currRock = 0;
+    int x, y;
+    int numSettledRocks = 0;
+    int moveIndex = 0;
+
+    while (true)
+    {
+        const char move = input[moveIndex];
+        moveIndex = (moveIndex + 1) % input.size();
+
+        if (spawnNewRock)
+        {
+            x = 2;
+            y = top() - 3 - rocks[currRock].height;
+            spawnNewRock = false;
+        }
+
+        Rock const& rock = rocks[currRock];
+
+        if (move == '<')
+        {
+            const bool canMove = std::all_of(rock.blocks.cbegin(),
+                                             rock.blocks.cend(),
+                                             [&](auto const& block) {
+                                                 return testSpace(x + block.first - 1,
+                                                                  y + block.second);
+                                             });
+
+            if (canMove)
+                x--;
+        }
+        else
+        {
+            const bool canMove = std::all_of(rock.blocks.cbegin(),
+                                             rock.blocks.cend(),
+                                             [&](auto const& block) {
+                                                 return testSpace(x + block.first + 1,
+                                                                  y + block.second);
+                                             });
+
+            if (canMove)
+                x++;
+        }
+
+        const bool canMove = std::all_of(rock.blocks.cbegin(),
+                                         rock.blocks.cend(),
+                                         [&](auto const& block) {
+                                             return testSpace(x + block.first,
+                                                              y + block.second + 1);
+                                         });
+
+        if (canMove)
+        {
+            ++y;
+        }
+        else
+        {
+            for (auto const& block : rock.blocks)
+                fixed.insert(std::make_pair(block.first + x, block.second + y));
+
+            if (++numSettledRocks == 2022)
+            {
+                std::cout << -top();
+                exit(EXIT_SUCCESS);
+            }
+
+            spawnNewRock = true;
+            currRock = (currRock + 1) % rocks.size();
+        }
+    }
+}
