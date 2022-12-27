f4e15bece0983a70eb269dc507ec72b9b2721eb5
Colin Barry <colinbarry@gmail.com>
2022-12-27 20:35:55 +0000

    feat: solve day 20 part 1

---
+++ 20/part1.cpp
@@ -0,0 +1,60 @@
+// g++ -std=c++17 part1.cpp && ./a.out < test
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using ValAndInitPos = std::pair<int, int>;
+
+int main()
+{
+    std::string s;
+    std::vector<ValAndInitPos> vec;
+
+    int i = 0;
+    while (std::getline(std::cin, s))
+        vec.push_back({stoi(s), i++});
+
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        auto const it = std::find_if(vec.begin(), vec.end(), [&](auto const& each) {
+            return each.second == i;
+        });
+
+        int const index = std::distance(vec.begin(), it);
+        int const val = it->first;
+        int newPos = (index + val) % static_cast<int>(vec.size() - 1);
+        if (newPos == 0)
+        {
+            newPos = vec.size() - 1;
+        }
+        else if (newPos < 0)
+        {
+            newPos += vec.size() - 1;
+        }
+
+        if (newPos > index)
+        {
+            std::rotate(it, std::next(it), vec.begin() + newPos + 1);
+        }
+        else
+        {
+            std::rotate(vec.begin() + newPos, it, std::next(it));
+        }
+    }
+
+    int const zeroIndex
+        = std::distance(vec.cbegin(),
+                        std::find_if(vec.cbegin(), vec.cend(), [](auto&& each) {
+                            return each.first == 0;
+                        }));
+
+    int const result = vec[(zeroIndex + 1000) % vec.size()].first
+                       + vec[(zeroIndex + 2000) % vec.size()].first
+                       + vec[(zeroIndex + 3000) % vec.size()].first;
+
+    std::cout << result << "\n";
+}
